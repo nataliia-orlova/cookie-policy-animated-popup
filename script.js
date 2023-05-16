f5940e1e7c0bb352abfa389a1245fdce8c9367c7
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2023-05-16 13:37:12 +0200

    add animation

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-gsap.fromTo('.text', { opacity: 0, y: 60 }, { opacity: 1, y: 0, duration: 1 });
+const tl = gsap.timeline({
+    defaults: { duration: 0.75, ease: 'Power1.easeOut' },
+});
+
+tl.fromTo(
+    '.cookie-container',
+    { scale: 0 },
+    { scale: 1, ease: 'Elastic.easeOut(1, 0.4)', duration: 1.5 }
+);
+tl.fromTo(
+    '.cookie',
+    { opacity: 0, x: -50, rotation: '-45deg' },
+    { opacity: 1, x: 0, rotation: '0deg' },
+    '<50%'
+);
+tl.fromTo('.text', { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, '<');
+
+//Cookie jump
+tl.fromTo(
+    '.cookie',
+    { y: 0, rotation: '0deg' },
+    {
+        y: -20,
+        rotation: '-25deg',
+        yoyo: true,
+        repeat: -1,
+    }
+);
+tl.fromTo('#Crumbs', { y: 0 }, { y: -60, yoyo: true, repeat: -1 }, '<');
+
+//Fading out
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', {
+        opacity: 0,
+        y: 100,
+        duration: 0.75,
+        ease: 'Power1.easeOut',
+    });
+});
